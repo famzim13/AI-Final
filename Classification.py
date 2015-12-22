5004b3430a2eff225c4f0d45d73c3d435cb28200
Alan Zimmer <azimmer1992@gmail.com>
2015-12-22 09:53:02 -0500

    SVM classifier complete, adding other project specs

--- Classification.py
+++ Classification.py
@@ -15,11 +15,11 @@ def line_split( line ):
     split_line = line.split(' ')
     return split_line[0], int(split_line[1])
 
-# Extract features from the set of training images
+# Load the training set of images into memory, convert them to gray scale
 def load_training_set( file ):
     images = []
     classify = []
-    img_size = [200, 200]
+    img_size = [250, 250]
     img_classify = 0
 
     try:
@@ -34,6 +34,20 @@ def load_training_set( file ):
 
     return images, classify
 
+def load_unknown_set( file ):
+    images = []
+    img_size = [250, 250]
+
+    try:
+        for line in file:
+            img = imread(line, as_grey=True)
+            img = resize(img, img_size)
+            images.append(img.flatten())
+    finally:
+        file.close()
+
+    return images
+
 def hog_svm_classifier( images, classify ):
     return
 
@@ -44,7 +58,7 @@ def pca_svm_classifier( images, classify ):
             images, classify, test_size=0.25, random_state=42)
 
         pca = RandomizedPCA(n_components=len(X_train), whiten=True).fit(X_train)
-        eigen_images = pca.components_.reshape((len(X_train), 200, 200))
+        eigen_images = pca.components_.reshape((len(X_train), 250, 250))
         X_train_pca = pca.transform(X_train)
         X_test_pca = pca.transform(X_test)
 
@@ -61,37 +75,36 @@ def k_near_svm_classifier( images, classify ):
     return
 
 def svm_classifier( images, classify ):
-    sum_score = 0
-    for _ in range(5):
-        X_train, X_test, y_train, y_test = train_test_split(
-            images, classify, test_size=0.25, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        images, classify, test_size=0.25)
 
-        clf = SVC(kernel='linear', class_weight='balanced')
-        clf = clf.fit(X_train, y_train)
-        sum_score += clf.score(X_test, y_test)
+    clf = SVC(kernel='linear', class_weight='balanced')
+    clf = clf.fit(X_train, y_train)
 
-        y_pred = clf.predict(X_test)
-        print( y_test )
-        print(classification_report(y_test, y_pred, target_names=['0', '1']))
-
-    print( sum_score/5 )
 
 if len(sys.argv) != 5:
     print( 'There was ' + str(len(sys.argv)-1) + ' arguements' )
     print( 'Four arguements must be entered, 2 input text files, 1 output text file and an algorithm identifier' )
 
 else:
+    # Open the entered files
     trainer_set = open(sys.argv[1], 'r')
-    images, classify = load_training_set( trainer_set )
+    unknown_set = open(sys.argv[2], 'r')
+    classified_set = open(sys.argv[3], 'w')
+
+    # Load the training data
+    training_images, classify = load_training_set( trainer_set )
     print( "Images Loaded" )
+
+
     if int(sys.argv[4]) > 4 or int(sys.argv[4]) < 1:
         print( "Algorithm identifier must be between 1-4" )
     else:
         if int(sys.argv[4]) == 1:
-            hog_svm_classifier(images, classify)
+            hog_svm_classifier(training_images, classify)
         elif int(sys.argv[4]) == 2:
-            pca_svm_classifier(images, classify)
+            pca_svm_classifier(training_images, classify)
         elif int(sys.argv[4]) == 3:
-            k_near_svm_classifier(images, classify)
+            k_near_svm_classifier(training_images, classify)
         elif int(sys.argv[4]) == 4:
-            svm_classifier(images, classify)
+            svm_classifier(training_images, classify)
