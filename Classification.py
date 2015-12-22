2018e479fe89b6d5127ba7747eb666d9ab2b655f
Alan Zimmer <azimmer1992@gmail.com>
2015-12-21 23:01:11 -0500

    Updated to include PCA SVM classifyer

--- Classification.py
+++ Classification.py
@@ -4,9 +4,13 @@ import numpy as np
 from skimage import color
 from skimage.io import imread
 from skimage.transform import resize
-from sklearn.svm import SVC
-from sklearn.feature_extraction.image import extract_patches_2d
+
+from sklearn.metrics import classification_report
+from sklearn.cross_validation import train_test_split
 from sklearn.decomposition import RandomizedPCA
+from sklearn.feature_extraction.image import extract_patches_2d
+from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
 
 # Split input line from the training text file
 def line_split( line ):
@@ -17,17 +21,16 @@ def line_split( line ):
 def load_training_set( file ):
     images = []
     classify = []
-    img_size = [250, 250]
+    img_size = [200, 200]
     img_classify = 0
 
     try:
         for line in file:
             img_url, img_classify = line_split( line )
             classify = np.hstack((classify, img_classify))
-            img = imread(img_url)
-            img = color.rgb2gray(img)
+            img = imread(img_url, as_grey=True)
             img = resize(img, img_size)
-            images.append(img)
+            images.append(img.flatten())
     finally:
         file.close()
 
@@ -36,25 +39,30 @@ def load_training_set( file ):
 def hog_feature_extraction( images, classify ):
     return
 
-def load_files( file_set ):
-    default_slice = (slice(0, 250), slice(0, 250))
+def pca_feature_extraction( images, classify ):
+    X_train, X_test, y_train, y_test = train_test_split(
+        images, classify, test_size=0.25, random_state=42)
+    print( "Train and test set" )
 
-    h_slice, w_slice = default_slice
-    h = (h_slice.stop - h_slice.start) // (h_slice.step or 1)
-    w = (w_slice.stop - w_slice.start) // (w_slice.step or 1)
+    pca = RandomizedPCA(n_components=len(X_train), whiten=True).fit(X_train)
+    print( "PCA fitted" )
+    eigen_images = pca.components_.reshape((len(X_train), 200, 200))
+    print( "Eigen images created" )
+    X_train_pca = pca.transform(X_train)
+    print( "Train pca created" )
+    X_test_pca = pca.transform(X_test)
+    print( "Test pca created" )
+    print(X_test_pca)
 
-    n_faces = len( file_set )
-    faces = np.zeros((n_faces, h, w), dtype=np.float32)
+    clf = SVC(kernel='linear', class_weight='balanced')
+    print( "SVC created" )
+    clf = clf.fit(X_train_pca, y_train)
+    print( "SVC fitted" )
 
-    for i, file_set in enumerate( file_set ):
-        img = imread( file_set )
-        face = np.asarray(img[default_slice], dtype=np.float32)
-        face /= 255.0
-        face = imresize(face, [250, 250])
-        face = face.mean(axis=2)
-        faces[i, ...] = face
+    y_pred = clf.predict(X_test_pca)
+    print(y_pred)
+    print(classification_report(y_test, y_pred, target_names=['0','1']))
 
-    return faces
 
 def classified_images( classified_set, output_loaction ):
     return
@@ -66,23 +74,15 @@ if len(sys.argv) != 5:
 else:
     trainer_set = open(sys.argv[1], 'r')
     images, classify = load_training_set( trainer_set )
-#    print( imread(zero_trainer_set[0]))
-#    zero_faces = load_files( zero_trainer_set )
-#    one_faces = load_files( one_trainer_set )
-#    zero_X = zero_faces.data
-#    zero_n_featues = zero_X.shape[1]
-#    one_X = one_faces.data
-#    one_n_features = one_X.shape[1]
-#    pca.fit(zero_faces.data)
+    print( "Images Loaded" )
     if int(sys.argv[4]) > 4 or int(sys.argv[4]) < 1:
         print( "Algorithm identifier must be between 1-4" )
     else:
         if int(sys.argv[4]) == 1:
             print(1)
         elif int(sys.argv[4]) == 2:
-            print(2)
+            pca_feature_extraction(images, classify)
         elif int(sys.argv[4]) == 3:
             print(3)
         elif int(sys.argv[4]) == 4:
             print(4)
-#        classified_images( classified_set, sys.argv[4] )
